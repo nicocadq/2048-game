efcf8f30b5d150a728a3ab67b1a2d203afe344dc
nicocadq <nmachado.camejo@gmail.com>
2021-09-13 13:49:59 -0300

    Add reset button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 import { Game } from 'components/Game';
 
 import './App.css';
 
-const App = () => <Game />;
+const App = () => {
+  const [gameKey, setGameKey] = useState(new Date().toISOString());
+
+  const onReset = () => setGameKey(new Date().toISOString());
+
+  return (
+    <div>
+      <button type="button" onClick={onReset}>
+        Reset
+      </button>
+      <Game key={gameKey} />
+    </div>
+  );
+};
 
 export default App;
